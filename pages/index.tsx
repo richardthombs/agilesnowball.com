11785ec3dbc1afab1da159979f443a7b942e640a
Richard Thombs <richard@gearstone.uk>
2021-03-01 19:35:23 +0000

    fix external link

--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function HomePage({ allPosts }) {
 				</h1>
 				<div className="text-gray-600 mb-16">
 					I'm a developer and have been ever since I got my first computer back in 1982 (a BBC model "A").
-					I've beaten <a className="font-medium" href="https://grack.com/demos/adventure" rel="nofollow" target="new">Colossal Cave</a>,
+					I've beaten <a className="font-medium" href="https://grack.com/demos/adventure" rel="nofollow" target="_blank">Colossal Cave</a>,
 					dabbled in a bit of MUD and even helped email addresses change from ! to @.
 				</div>
 
