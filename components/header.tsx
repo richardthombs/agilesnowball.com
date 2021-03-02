92cc207ab5dd71a4d52e03f1ae8ecfafbad788ac
Richard Thombs <richard@gearstone.uk>
2021-03-02 16:45:55 +0000

    Minor tweakage

--- components/header.tsx
+++ components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
 	return (
-		<header className="px-4 py-8 sm:px-16 bg-gray-50 text-gray-800 flex space-x-4 sm:space-x-8 items-center border-b border-gray-200 shadow">
+		<header className="px-4 py-8 sm:px-16 bg-gray-50 text-gray-800 flex space-x-4 sm:space-x-8 items-center border-b border-gray-200">
 
 			<h1 className="text-2xl sm:text-4xl font-bold tracking-tight w-full">
 				<Link href="/">
